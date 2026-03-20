08a12c9e06fea27f3fe72c3c16fb74107a1cbb5a
phamha98 <phamtranquangha@gmail.com>
2026-03-20 23:35:05 +0700

    add

--- Example.tsx
+++ Example.tsx
@@ -1,12 +1,13 @@
 import { StyleSheet, View } from 'react-native'
 import React from 'react'
-import { Text, Block } from './src/index'
+import { Text, Block,Icon } from './src/index'
 
 export default function Example() {
   return (
     <View>
       <Block pad-40 />
       <Text pink color='red' />
+      <Icon name='red'/>
     </View>
   )
 }
