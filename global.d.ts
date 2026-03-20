8de59ee6d8f0eceafe949a6eb7346b6d1cc78106
phamha98 <phamtranquangha@gmail.com>
2026-03-20 21:58:34 +0700

    2.0.0

--- global.d.ts
+++
@@ -1,2 +0,0 @@
-// src/types/global.d.ts
-declare module 'react-native-vector-icons/*';
---
+++ src/types/react-native-vector-icons.d.ts
@@ -0,0 +1,12 @@
+declare module 'react-native-vector-icons/*' {
+  import { ComponentType } from 'react';
+  import { TextProps } from 'react-native';
+
+  const Icon: ComponentType<TextProps & {
+    name: string;
+    size?: number;
+    color?: string;
+  }>;
+
+  export default Icon;
+}
