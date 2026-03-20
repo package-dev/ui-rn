51f9111dac5366ea1e5b4f1e5e9525085428d7de
phamha98 <phamtranquangha@gmail.com>
2026-03-20 23:21:15 +0700

    2.0.4

---
+++ src/Icon/PropsStyle.d.ts
@@ -0,0 +1,15 @@
+import { RNVectorIcon } from "./index";
+import { FlexStyle, StyleProp, TextStyle, ViewStyle } from 'react-native';
+import { MakeProp } from "./makeProps";
+export interface Props extends MakeProp {
+    name: string;
+    size?: number;
+    type?: keyof typeof RNVectorIcon;
+    onPress?: () => void;
+    styleContainer?: StyleProp<ViewStyle>;
+    style?: StyleProp<TextStyle>;
+    alignSelf?: FlexStyle['alignSelf'];
+    disabled?: boolean;
+    activeOpacity?: number;
+    color?: string;
+}
--- src/Icon/makeProps.js
+++ src/Icon/makeProps.js
@@ -1,20 +1,16 @@
-import { StyleSheet, ViewStyle } from "react-native";
-
-enum COLORS {
-    white = 1,
-    red,
-    blue,
-    black,
-    green,
-    yellow,
-    pink,
-    gray,
-    primary,
-}
-
-type ColorKey = keyof typeof COLORS;
-type ColorModifiers = Partial<Record<ColorKey, boolean>>;
-
+import { StyleSheet } from "react-native";
+var COLORS;
+(function (COLORS) {
+    COLORS[COLORS["white"] = 1] = "white";
+    COLORS[COLORS["red"] = 2] = "red";
+    COLORS[COLORS["blue"] = 3] = "blue";
+    COLORS[COLORS["black"] = 4] = "black";
+    COLORS[COLORS["green"] = 5] = "green";
+    COLORS[COLORS["yellow"] = 6] = "yellow";
+    COLORS[COLORS["pink"] = 7] = "pink";
+    COLORS[COLORS["gray"] = 8] = "gray";
+    COLORS[COLORS["primary"] = 9] = "primary";
+})(COLORS || (COLORS = {}));
 const PROPS = {
     mar: "margin",
     marH: "marginHorizontal",
@@ -26,64 +22,41 @@ const PROPS = {
     zIndex: "zIndex",
     opacity: "opacity",
     size: "size",
-} as const;
-
-export type ShortKey = keyof typeof PROPS;
-
-export type Convert<T extends string> = Partial<
-    Record<T, boolean | number | string>
->;
-
-export type MakeProp = Convert<ShortKey> & ColorModifiers;
-
-/** type guards */
-const isColorKey = (key: string): key is ColorKey => key in COLORS;
-const isShortKey = (key: string): key is ShortKey =>
-    key in PROPS;
-
-type Result = {
-    size: number;
-    color: string;
-    name: string;
-    style: ViewStyle;
 };
-
-export const makeProps = (props: Record<string, any>): Result => {
-    const style: ViewStyle = {};
-
-    const res: Omit<Result, "style"> = {
+/** type guards */
+const isColorKey = (key) => key in COLORS;
+const isShortKey = (key) => key in PROPS;
+export const makeProps = (props) => {
+    const style = {};
+    const res = {
         size: props.size ?? 23,
         color: props.color ?? "gray",
         name: props.name ?? "home",
     };
-
     Object.keys(props).forEach((key) => {
         const match = key.match(/^(\w+)-(\w+)$/);
-
         // color modifier
         if (isColorKey(key)) {
             res.color = key;
             return;
         }
-
         if (match) {
             const rawKey = match[1];
             const rawValue = match[2];
-
             const value = isNaN(Number(rawValue))
                 ? rawValue
                 : Number(rawValue);
-
             if (rawKey === "size") {
                 res.size = Number(rawValue);
-            } else if (rawKey === "color") {
+            }
+            else if (rawKey === "color") {
                 res.color = rawValue;
-            } else if (isShortKey(rawKey)) {
-                (style as any)[PROPS[rawKey]] = value;
+            }
+            else if (isShortKey(rawKey)) {
+                style[PROPS[rawKey]] = value;
             }
         }
     });
-
     return {
         ...res,
         style: StyleSheet.create({ style }).style,
